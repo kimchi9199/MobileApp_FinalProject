7cf6a79c84a3ed74f3993c26661ba88b83678b85
KimChi9199 <19521283@gm.uit.edu.vn>
2023-05-21 16:34:46 +0700

    connect_server _ send_and_receive_video_stream

--- Mobile_Final/app/src/main/java/com/example/mobileapp_final/Stream_Video_Activity.java
+++ Mobile_Final/app/src/main/java/com/example/mobileapp_final/Stream_Video_Activity.java
@@ -3,14 +3,80 @@ package com.example.mobileapp_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Stream_Video_Activity extends AppCompatActivity {
+    private ImageView mimageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stream_video);
+        mimageView = findViewById(R.id.Iv_Streamvideo);
+
+        final String SERVER_IP = "192.168.1.6"; // Server IP address
+        final int SERVER_PORT = 9999; // Server port number
+        final int BUFFER_SIZE = 65536; // Buffer size in bytes
+        final int CLIENT_PORT = 9090;
+
+
+        Thread receiveVideoThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DatagramSocket udpSocket = null;
+                 try {
+
+                     udpSocket = new DatagramSocket(CLIENT_PORT);
+
+                     byte[] buffer = new byte[BUFFER_SIZE];
+                     DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                     String message = "hello";
+                     byte[] data = message.getBytes();
+                     InetAddress serverAddress = null;
+                     serverAddress = InetAddress.getByName(SERVER_IP);
+
+                     DatagramPacket sendPacket = new DatagramPacket(data, data.length, serverAddress, SERVER_PORT);
+                     udpSocket.send(sendPacket);
+                } catch (IOException e)
+                {
+                    throw new RuntimeException(e);
+                }
+
+//                Received video frame from server and display them
+                try {
+                    byte[] videoBuffer = new byte[BUFFER_SIZE];
+                    DatagramPacket videoFramePacket = new DatagramPacket(videoBuffer, videoBuffer.length);
+                    while (true) {
+                        udpSocket.receive(videoFramePacket);
+                        String lText = new String(videoBuffer, 0, videoFramePacket.getLength());
+                        byte[] decodeDataImg = Base64.decode(lText, Base64.DEFAULT);
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(decodeDataImg, 0, decodeDataImg.length);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mimageView.setImageBitmap(bitmap);
+                            }
+                        });
+
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+
+        });
+        receiveVideoThread.start();
+
 
     }
 }
---
+++ Mobile_Final/server.py
@@ -0,0 +1,40 @@
+import base64
+import socket
+
+import cv2
+import imutils
+
+camera = cv2.VideoCapture(0) # 0 select first camera
+
+# UDP (realtime)
+BUFF_SIZE = 65536
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print(host_ip)
+port = 9999
+# socket_address = (host_ip, port)
+# server_socket.bind(socket_address)
+socket_address = ('192.168.1.6', port)
+server_socket.bind(socket_address)
+print('Listening at: ', socket_address)
+
+while True:
+    msg, client_adr = server_socket.recvfrom(BUFF_SIZE)
+    print('GOT connection from', client_adr)
+    WIDTH = 400
+    while True:
+        _, frame = camera.read() #return 2 values, True: camera can read. False: can not read. Use when read video
+        frame = imutils.resize(frame, width=WIDTH)
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        message = base64.b64encode(buffer)
+        server_socket.sendto(message, client_adr)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+
