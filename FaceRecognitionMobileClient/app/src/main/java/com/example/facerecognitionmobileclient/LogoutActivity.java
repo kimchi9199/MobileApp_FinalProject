138d8a309a76880b6c30d8135a8c89b8bc6eb1bb
Thuytrang <19521037@gm.uit.edu.vn>
2023-05-25 12:40:52 +0700

    sign up login firebase commit

--- FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/LogoutActivity.java
+++ FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/LogoutActivity.java
@@ -50,7 +50,7 @@ public class LogoutActivity extends AppCompatActivity {
 
 
         user= auth.getCurrentUser();
-        showAllUserData();
+//        showAllUserData();
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/SignupActivity.java
+++ FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/SignupActivity.java
@@ -28,27 +28,27 @@ import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText signupemail, signuppassword, signupusername, signupphone;
+    EditText signupemail, signuppassword;
     Button btnSingup;
     FirebaseAuth mAuth;
-    ProgressBar progressBar;
+
     TextView loginRedirectText;
      FirebaseDatabase database;
     DatabaseReference reference;
 
 
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,13 +57,8 @@ public class SignupActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         signupemail = findViewById(R.id.signup_email);
         signuppassword = findViewById(R.id.signup_password);
-        signupusername = findViewById(R.id.signup_username);
-        signupphone = findViewById(R.id.signup_phone);
-        btnSingup = findViewById(R.id.btn_signup);
-        progressBar = findViewById(R.id.progressBar);
+        btnSingup = findViewById(R.id.signup_button);
         loginRedirectText = findViewById(R.id.loginRedirectText);
-
-
 
         loginRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,60 +71,28 @@ public class SignupActivity extends AppCompatActivity {
         btnSingup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                progressBar.setVisibility(View.VISIBLE);
-                database = FirebaseDatabase.getInstance();
-                reference = database.getReference("users");
-                mAuth = FirebaseAuth.getInstance();
-                String phone = signupphone.getText().toString();
-                String email = signupemail.getText().toString();
-                String username = signupusername.getText().toString();
-                String password = signuppassword.getText().toString();
-//                User user = new User( email, username, password,phone);
-//                reference.child(email).setValue(user);
-
-                // Check if email exists in the database
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(SignupActivity.this, "Please enter an email", Toast.LENGTH_SHORT).show();
-                    return;
+                String user = signupemail.getText().toString().trim();
+                String pass = signuppassword.getText().toString().trim();
+                if (user.isEmpty()){
+                    signupemail.setError("Email cannot be empty");
                 }
-                if (TextUtils.isEmpty(username)) {
-                    Toast.makeText(SignupActivity.this, "Please enter an username", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(SignupActivity.this, "Please enter an password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(phone)) {
-                    Toast.makeText(SignupActivity.this, "Please enter an phone", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                reference.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            // Email exists in the database
-                            Toast.makeText(SignupActivity.this, "Email already exists", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // Email does not exist, proceed with creating the new
-                            String encodedEmail = email.replace(".", ",");
-                            User user = new User(email, username, password, phone);
-                            reference.child(encodedEmail).setValue(user);
-                            Toast.makeText(SignupActivity.this, "You have signed up successfully!", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                            startActivity(intent);
+                if (pass.isEmpty()){
+                    signuppassword.setError("Password cannot be empty");
+                } else{
+                    mAuth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignupActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                            } else {
+                                Toast.makeText(SignupActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        // Handle the error
-                        Toast.makeText(SignupActivity.this, "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
+                    });
+                }
             }
+
         });
     }
 }
