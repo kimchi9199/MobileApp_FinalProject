d5b075496aeee87c924a92e0a38a7ba9a47d8978
Lương Trang <91070932+imchan01@users.noreply.github.com>
2023-06-03 12:44:29 +0700

    commit again

--- FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/User.java
+++ FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/User.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.facerecognitionmobileclient;
 
 public class User {
@@ -44,3 +45,51 @@ public class User {
     }
 
 }
+=======
+package com.example.facerecognitionmobileclient;
+
+public class User {
+    String  date, desc;
+    private String dataImage;
+    private String key;
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+
+    public String getDataImage() {
+        return dataImage;
+    }
+
+    public void setDataImage(String dataImage) {
+        this.dataImage = dataImage;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public User(String date, String desc, String dataImage) {
+        this.date = date;
+        this.desc = desc;
+        this.dataImage=dataImage;
+    }
+    public User() {
+    }
+
+}
+>>>>>>> Stashed changes
