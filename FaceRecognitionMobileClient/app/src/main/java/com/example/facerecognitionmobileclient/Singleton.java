538e5213c4fe4077a57b22662bbc185cd7e1ccf6
KimChi9199 <91075598+kimchi9199@users.noreply.github.com>
2023-06-03 16:40:59 +0700

    merged code

--- FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/Singleton.java
+++ FaceRecognitionMobileClient/app/src/main/java/com/example/facerecognitionmobileclient/Singleton.java
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 package com.example.facerecognitionmobileclient;
 
 public class Singleton {
@@ -26,32 +26,3 @@ public class Singleton {
         this.data = data;
     }
 }
-=======
-package com.example.facerecognitionmobileclient;
-
-public class Singleton {
-    private static Singleton instance;
-    private User data;
-    // Private constructor to prevent instantiation from outside the class
-    private Singleton() {
-        // Initialization code, if any
-    }
-
-    // Method to get the singleton instance
-    public static Singleton getInstance() {
-        if (instance == null) {
-            // Create a new instance if it doesn't exist
-            instance = new Singleton();
-        }
-        return instance;
-    }
-
-    public User getData() {
-        return data;
-    }
-
-    public void setData(User data) {
-        this.data = data;
-    }
-}
->>>>>>> Stashed changes
--- FaceRecognitionMobileClient/sdk/build/intermediates/cxx/Debug/yz4b4u4z/logs/x86/build_command_opencv_jni_shared.bat
+++ FaceRecognitionMobileClient/sdk/build/intermediates/cxx/Debug/yz4b4u4z/logs/x86/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
-"E:\\AndroidDev\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\Thai\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "E:\\Download\\FaceRecognition - Final\\FaceRecognition - Final\\FaceRecognitionMobileClient\\sdk\\.cxx\\Debug\\yz4b4u4z\\x86" ^
+  "D:\\Study\\Cirriculums\\Mobile-Developement\\KC\\FinalProject\\MobileApp_FinalProject\\FaceRecognitionMobileClient\\sdk\\.cxx\\Debug\\yz4b4u4z\\x86" ^
   opencv_jni_shared
